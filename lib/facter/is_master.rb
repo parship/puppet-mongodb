7fc50558a9e5300bb6a85e59f64b767190355c09
Bernd Eilers <bernd.eilers@parshipgroup.com>
2023-06-26 16:58:45 +0200

    SIRE-9538 adapt is_master fact to use mongosh instead of old mongo client Reviewed-by: muspelkat

--- lib/facter/is_master.rb
+++ lib/facter/is_master.rb
@@ -77,13 +77,13 @@ Facter.add('mongodb_is_master') do
       file = mongod_conf_file
       if file
         options = get_options_from_config(file)
-        e = File.exist?('/root/.mongorc.js') ? 'load(\'/root/.mongorc.js\'); ' : ''
+        e = File.exist?('/root/.mongoshrc.js') ? 'load(\'/root/.mongoshrc.js\'); ' : ''
 
         # Check if the mongodb server is responding:
-        Facter::Core::Execution.exec("mongo --quiet #{options} --eval \"#{e}EJSON.stringify(db.adminCommand({ ping: 1 }))\"")
+        Facter::Core::Execution.exec("mongosh --quiet #{options} --eval \"#{e}EJSON.stringify(db.adminCommand({ ping: 1 }))\"")
 
         if $CHILD_STATUS.success?
-          Facter::Core::Execution.exec("mongo --quiet #{options} --eval \"#{e}db.isMaster().ismaster\"")
+          Facter::Core::Execution.exec("mongosh --quiet #{options} --eval \"#{e}db.isMaster().ismaster\"")
         else
           'not_responding'
         end
